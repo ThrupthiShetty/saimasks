e1e1ec6e777981430cb3b3f8e9b3546c7bd4cabb
thrupthishetty <thrupthishetty@gmail.com>
2021-05-29 20:22:18 +0530

    added bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { RequestsListComponent } from './requests-list/requests-list.component';
 
@@ -7,7 +8,8 @@ import { RequestsListComponent } from './requests-list/requests-list.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'requests', component: RequestsListComponent},
-  { path: '', redirectTo: '/', pathMatch: 'full' }
+  { path: 'dashboard', component: DashboardComponent},
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
  
 ];
 
